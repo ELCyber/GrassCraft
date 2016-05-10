3315639395ccf79be4bc54bc608c61dd483e6185
Hiki <aneureka2@gmail.com>
2016-05-11 00:23:29 +0800

    写的差不多了。。吧。。
    不过感觉这代码风格得被人砍死hhhh

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -1,6 +1,6 @@
 package control;
 
-import Logi.TurnCounter;
+
 import dto.GameInfo;
 import dto.Grassman;
 import ui.mapPanel;
@@ -11,7 +11,9 @@ public class GameControl {
 	private Grassman[] mans;
 	private GameInfo info;
 	private Grassman man;
- 
+	private int xPosition = man.getXPosition();
+	private int yPosition = man.getYPosition();
+
 	public GameControl(mapPanel mapP, Grassman[] mans, GameInfo info) {
 		// 初始化，传入第一个grassman及所有grassman数组；
 		this.mapP = mapP;
@@ -19,17 +21,12 @@ public class GameControl {
 		this.info = info;
 		this.man = mans[0];
 	}
+
 	// 地图数组：0 表示 无人 1-6 表示 player0-5 7 表示 攻击范围
 	// Actions control
 	// 移动：原理（交换地图数组数值）
 
 	// 判断血量是否为0
-	public boolean isDead() {
-		if (this.man.getBlood() == 0) {
-			return true;
-		} else
-			return false;
-	}
 
 	// 移动：原理（交换地图数组数值） 判断（边界、体力值、撞到人（将移动到的地方地图数组数值是否!0））
 	public boolean KeyUp() {
@@ -38,12 +35,13 @@ public class GameControl {
 		info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 
 		if (this.isOverZone(man.getXPosition(), man.getYPosition())
-				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans) ) {
+				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans)) {
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 0;
 			man.setY(-1);
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 			return false;
 		} else {
+			this.man.setCure(this.man.getCure() - 2);
 			this.mapP.repaint();
 			return true;
 		}
@@ -54,12 +52,13 @@ public class GameControl {
 		man.setY(-1);
 		info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 		if (this.isOverZone(man.getXPosition(), man.getYPosition())
-				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans) ) {
+				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans)) {
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 0;
 			man.setY(1);
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 			return false;
 		} else {
+			this.man.setCure(this.man.getCure() - 2);
 			this.mapP.repaint();
 			return true;
 		}
@@ -70,12 +69,13 @@ public class GameControl {
 		man.setX(-1);
 		info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 		if (this.isOverZone(man.getXPosition(), man.getYPosition())
-				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans) ) {
+				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans)) {
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 0;
 			man.setX(1);
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 			return false;
 		} else {
+			this.man.setCure(this.man.getCure() - 2);
 			this.mapP.repaint();
 			return true;
 		}
@@ -86,33 +86,72 @@ public class GameControl {
 		man.setX(1);
 		info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 		if (this.isOverZone(man.getXPosition(), man.getYPosition())
-				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans) ) {
+				|| this.isCrashed(man.getXPosition(), man.getYPosition(), mans)) {
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 0;
 			man.setX(-1);
 			info.map[this.man.getXPosition()][this.man.getYPosition()] = 1;
 			return false;
 		} else {
+			this.man.setCure(this.man.getCure() - 2);
 			this.mapP.repaint();
 			return true;
 		}
 	}
 
 	// 攻击：判断：体力值 是不是攻击到（涉及到对成员数组的操作）
-	public boolean KeyOffendUp() {
-
-		return false;
+	// 说明：第一个判断（是不是攻击范围） 第二个判断（有没有 敌 人在，若有，扣血），最后体力-4
+	public void KeyOffendUp() {
+		for (int i = 0; i < 6; i++) {
+			if(man.getOx()[this.judgeWeapon()][i] != 0 || man.getOy()[this.judgeWeapon()][i] != 0){
+				if((this.numOfMan() < 3 && (info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]] >= 4))
+					|| this.numOfMan() > 2 && (info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]] < 4)){
+				mans[info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]]].setBlood
+				(mans[info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]]].getBlood() - 1);
+				}
+			}
+		}
+		this.man.setCure(this.man.getCure() - 4);
 	}
 
-	public boolean KeyOffendDown() {
-		return false;
+	public void KeyOffendDown() {
+		for (int i = 0; i < 6; i++) {
+			if(man.getOx()[this.judgeWeapon()][i] != 0 || man.getOy()[this.judgeWeapon()][i] != 0){
+				if((this.numOfMan() < 3 && (info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]] >= 4))
+					|| this.numOfMan() > 2 && (info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]] < 4)){
+				mans[info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]]].setBlood
+				(mans[info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]]].getBlood() - 1);
+				}
+			}
+		}
+		this.man.setCure(this.man.getCure() - 4);
 	}
 
-	public boolean KeyOffendLeft() {
-		return false;
+	public void KeyOffendLeft() {
+		for (int i = 0; i < 6; i++) {
+			if(man.getOx()[this.judgeWeapon()][i] != 0 || man.getOy()[this.judgeWeapon()][i] != 0){
+				if((this.numOfMan() < 3 && (info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]] >= 4))
+					|| this.numOfMan() > 2 && (info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]] < 4)){
+				mans[info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]]].setBlood
+				(mans[info.map[xPosition - man.getOx()[this.judgeWeapon()][i]][yPosition - man.getOy()[this.judgeWeapon()][i]]].getBlood() - 1);
+				}
+			}
+		}
+
+		this.man.setCure(this.man.getCure() - 4);
 	}
 
-	public boolean KeyOffendRight() {
-		return false;
+	public void KeyOffendRight() {
+		for (int i = 0; i < 6; i++) {
+			if(man.getOx()[this.judgeWeapon()][i] != 0 || man.getOy()[this.judgeWeapon()][i] != 0){
+				if((this.numOfMan() < 3 && (info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]] >= 4))
+					|| this.numOfMan() > 2 && (info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]] < 4)){
+				mans[info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]]].setBlood
+				(mans[info.map[xPosition + man.getOx()[this.judgeWeapon()][i]][yPosition + man.getOy()[this.judgeWeapon()][i]]].getBlood() - 1);
+				}
+			}
+		}
+
+		this.man.setCure(this.man.getCure() - 4);
 	}
 
 	// 确认：判断是否回合完毕，若未， 回合+1
@@ -122,7 +161,7 @@ public class GameControl {
 			return 1;
 		}
 		info.setTurns();
-        this.getNextPlayer();
+		this.getNextPlayer();
 		return 0;
 	}
 
@@ -199,5 +238,31 @@ public class GameControl {
 		} else {
 			return false;
 		}
+	}
+
+	public int judgeWeapon() {
+		for (int i = 0; i < 6; i++) {
+			if (this.man == mans[i]) {
+				switch (i) {
+				case 0 | 3:
+					return 0;
+				case 1 | 4:
+					return 1;
+				case 2 | 5:
+					return 2;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public int numOfMan() {
+		for (int i = 0; i < 6; i++) {
+			if (this.man == mans[i]) {
+				return i;
+			}
+		}
+
+		return -1;
 	}
 }
