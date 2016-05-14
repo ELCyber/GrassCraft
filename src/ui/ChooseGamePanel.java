8818ffd84549752b2823e531c1e83faaa89cffc2
cccyf <731767381@qq.com>
2016-05-14 20:25:42 +0800

    选择模式图片

--- src/ui/ChooseGamePanel.java
+++ src/ui/ChooseGamePanel.java
@@ -3,6 +3,7 @@ package ui;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -15,6 +16,12 @@ public class ChooseGamePanel extends JPanel implements MouseListener {
 	 JButton doubleButton;
 	 JButton backButton;
 	 GameButton gameButton;
+	 ImageIcon iconSingle =new ImageIcon("Graphics/Button/single.png");
+	 ImageIcon iconDouble =new ImageIcon("Graphics/Button/double.png");
+	 ImageIcon iconBack =new ImageIcon("Graphics/Button/back.png");
+	 ImageIcon iconBigSingle =new ImageIcon("Graphics/Button/bigSingle.png");
+	 ImageIcon iconBigDouble =new ImageIcon("Graphics/Button/bigDouble.png");
+	 ImageIcon iconBigBack =new ImageIcon("Graphics/Button/bigBack.png");
 	 public ChooseGamePanel(FrameGame frame,GameButton gameButton){
 		 this.setLayout(null);
 		 this.setOpaque(false);
@@ -25,35 +32,36 @@ public class ChooseGamePanel extends JPanel implements MouseListener {
 		 this.add(createBackButton());
 	 }
 		public JButton createSingleButton() {
-			singleButton = new JButton("single");
+			
+			singleButton = new JButton();
 			singleButton.setBorder(null);
-			singleButton.setBounds(500,300, 100, 50);
+			singleButton.setBounds(520,300, 200, 50);
 			singleButton.setContentAreaFilled(true);
 			singleButton.setBorderPainted(false);
-//			singleButton.setIcon(iconStart);
-//			singleButton.setRolloverIcon(iconStartEntered);
+			singleButton.setIcon(iconSingle);
+			singleButton.setRolloverIcon(iconBigSingle);
 			singleButton.addMouseListener(this);
 			return singleButton;
 		}	
 		public JButton createDoubleButton() {
-			doubleButton = new JButton("double");
+			doubleButton = new JButton();
 			doubleButton.setBorder(null);
-			doubleButton.setBounds(500,400, 100, 50);
+			doubleButton.setBounds(520,400, 200, 50);
 			doubleButton.setContentAreaFilled(true);
 			doubleButton.setBorderPainted(false);
-//			doubleButton.setIcon(iconStart);
-//			doubleButton.setRolloverIcon(iconStartEntered);
+			doubleButton.setIcon(iconDouble);
+			doubleButton.setRolloverIcon(iconBigDouble);
 			doubleButton.addMouseListener(this);
 			return doubleButton;
 		}
 		public JButton createBackButton() {
-			backButton = new JButton("back");
+			backButton = new JButton();
 			backButton.setBorder(null);
-			backButton.setBounds(500,500, 100, 50);
+			backButton.setBounds(520,500, 200, 50);
 			backButton.setContentAreaFilled(true);
 			backButton.setBorderPainted(false);
-//			backButton.setIcon(iconStart);
-//			backButton.setRolloverIcon(iconStartEntered);
+			backButton.setIcon(iconBack);
+			backButton.setRolloverIcon(iconBigBack);
 			backButton.addMouseListener(this);
 			return backButton;
 		}
